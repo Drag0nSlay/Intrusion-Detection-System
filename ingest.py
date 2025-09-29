a7d4b09797380e2f58938771d40791d44ab74799
Drag0nSlay <95128314+Drag0nSlay@users.noreply.github.com>
2025-09-29 20:40:40 +0530

    Delete ingest.py

--- ingest.py
+++
@@ -1,8 +0,0 @@
-import json, requests
-
-# Suricata logs file (eve.json) open karo
-with open("/var/log/suricata/eve.json") as f:
-    for line in f:
-        data = json.loads(line)
-        if "alert" in data:                # sirf alerts filter karo
-            requests.post("http://10.70.18.25:5000/ingest", json=data)
